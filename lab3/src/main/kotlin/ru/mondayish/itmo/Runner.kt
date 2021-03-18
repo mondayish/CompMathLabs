e4278a82fe853e5bfd0cbcc2cc0a50ec30f9dc8c
mondayish <misha140501@mail.ru>
2021-03-18 15:43:00 +0300

    Small fix

--- lab3/src/main/kotlin/ru/mondayish/itmo/Runner.kt
+++ lab3/src/main/kotlin/ru/mondayish/itmo/Runner.kt
@@ -8,6 +8,7 @@ import ru.mondayish.itmo.models.MathFunction
 import ru.mondayish.itmo.models.UserInput
 import kotlin.math.cos
 import kotlin.math.pow
+import kotlin.math.sqrt
 
 
 const val SPLIT_NUMBER: Int = 4
@@ -15,7 +16,7 @@ val AVAILABLE_FUNCTIONS: Array<MathFunction> = arrayOf(
     MathFunction("-2x^3 - 4x^2 + 8x - 4") { x: Double -> -2 * x.pow(3) - 4 * x * x + 8 * x - 4 },
     MathFunction("x^2 + 4x - 17") { x: Double -> x * x + 4 * x - 17 },
     MathFunction("cos(2x)/(x^2 + 1)") { x: Double -> cos(2 * x) / (x * x + 1) },
-    MathFunction("sqrt(x^4/7 + 3)") { x: Double -> x.pow(4) / 7 + 3 }
+    MathFunction("sqrt(x^4/7 + 3)") { x: Double -> sqrt(x.pow(4) / 7 + 3) }
 )
 
 fun main(args: Array<String>) {
