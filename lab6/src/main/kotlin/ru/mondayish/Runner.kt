2cfdc55afbb2ce4596bc27029ff45e3fc98c6d16
mondayish <misha140501@mail.ru>
2021-05-26 00:43:04 +0300

    Init lab6

---
+++ lab6/src/main/kotlin/ru/mondayish/Runner.kt
@@ -0,0 +1,6 @@
+package ru.mondayish
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ lab6/src/test/kotlin/ru/mondayish/HelloTest.kt
@@ -0,0 +1,8 @@
+package ru.mondayish
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
